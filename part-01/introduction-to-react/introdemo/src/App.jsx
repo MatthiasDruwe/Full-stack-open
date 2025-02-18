d3aeef61ddf6a6ae8d1ccaee9322879b7146d455
Matthias Druwé <matthias.druwe@gmail.com>
2025-02-18 20:59:23 +0100

    Finish 1a

--- part-01/introduction-to-react/introdemo/src/App.jsx
+++ part-01/introduction-to-react/introdemo/src/App.jsx
@@ -1,16 +1,20 @@
-const Hello = () => {
+const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello World</p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
   )
 }
 
 const app = () => {
+  const name = "Peter"
+  const age = 10
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello />
+      <Hello name="Maya" age={10+10}/>
+      <Hello name={name} age={age}/>
     </div>
   )
 }
