4d41ee48c3933785156d3c97cc0116da36d27549
matthias.druwe <matthias.druwe@odisee.be>
2025-04-01 09:01:51 +0200

    ex2.5

--- part-02/exercises/courseinfo/src/App.jsx
+++ part-02/exercises/courseinfo/src/App.jsx
@@ -1,32 +1,4 @@
-const Header = (props) => <h2>{props.course}</h2>;
-
-const Content = (props) => (
-  <div>
-    {props.parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </div>
-);
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-);
-
-const Total = (props) => <strong>Number of exercises {props.total}</strong>;
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total
-        total={course.parts.reduce((acc, part) => acc + part.exercises, 0)}
-      />
-    </div>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
