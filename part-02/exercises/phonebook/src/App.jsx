8c40aa8469041d152465b9e3ed9e93d93e44f561
Matthias Druwé <matthias.druwe@gmail.com>
2025-05-01 22:07:10 +0200

    WIP phonebook ex 2.6

---
+++ part-02/exercises/phonebook/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import { Person } from './components/Person'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+
+  const [newName, setNewName] = useState('')
+  const handleNewNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input onChange={handleNewNameChange} value={newName} />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+
+      <ul>{
+        persons.map(person =>
+          <Person key={person.name} person={person} />
+        )}
+      </ul>
+
+    </div>
+  )
+
+}
+
+export default App
